8d7841238eb10b43e30c3098b5708f922f1cab83
Sachith Akkadka <149311903+sachith-akkadka@users.noreply.github.com>
2025-07-08 18:58:20 +0530

    Latest Project Update 08-07-2005

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,15 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjGC9K4cSbmDOwav1j9m4rx36oKB7FUjs",
-  authDomain: "g-remind-41d9a.firebaseapp.com",
-  projectId: "g-remind-41d9a",
-  storageBucket: "g-remind-41d9a.firebasestorage.app",
-  messagingSenderId: "961638120180",
-  appId: "1:961638120180:android:a7a885255a6617a371bca1"
+  apiKey: "AIzaSyDbEiHntp7lTPktQymM8_-AidubXENUpjw",
+  authDomain: "g-remind-7aacb.firebaseapp.com",
+  projectId: "g-remind-7aacb",
+  storageBucket: "g-remind-7aacb.firebasestorage.app",
+  messagingSenderId: "809725747088",
+  appId: "1:809725747088:android:9b900e84bd17f962f25d15"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
+
